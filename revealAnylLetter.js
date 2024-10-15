e0f9984d340b8df466e6a1946bf15563d90524e8
boris <boriskirenga@gmail.com>
2024-10-15 03:42:07 -0700

    a file of code challenges updated

---
+++ revealAnylLetter.js
@@ -0,0 +1,11 @@
+
+const anyName = "samuel";
+
+console.log(anyName.charAt(Math.floor(Math.random()* anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random()* anyName.length)));
+
+
+
+
+
+// code challenge
